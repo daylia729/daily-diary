c7ea8e2f7363919d2f1c3fc0581fcc00045dc1df
daylia729 <598247125@qq.com>
2025-04-10 11:53:46 +0800

    Add .nojekyll file to disable Jekyll

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './', // 修改为相对路径
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  }
 })
