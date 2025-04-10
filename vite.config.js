18c7e0966ab734b4bf5078398f7fd0f241629652
daylia729 <598247125@qq.com>
2025-04-10 10:13:12 +0800

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/daily-diary/', // 确保这里的名称与您的仓库名一致
+  base: './', // 修改为相对路径
 })
